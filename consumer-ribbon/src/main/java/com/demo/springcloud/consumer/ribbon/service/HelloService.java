2463070f4da843c5987c85458cdd30e6d3a97061
zhigang.huang <zhigang.huang@wwwarehouse.com>
2018-02-05 20:44:39 +0800

    目录调整

--- consumer-ribbon/src/main/java/com/demo/springcloud/consumer/ribbon/service/HelloService.java
+++ consumer-ribbon/src/main/java/com/demo/springcloud/consumer/ribbon/service/HelloService.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
+
 /**
  * Created by zhigang.huang on 2018/2/5.
  */
@@ -17,7 +19,8 @@ public class HelloService {
     RestTemplate restTemplate;
 
     public String hiService(String name) {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        System.out.println("hiService--" + (new Date()));
+        ServiceInstance serviceInstance = loadBalancerClient.choose("client-eureka");
         String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hi?name=" + name;
         System.out.println(url);
 
@@ -25,6 +28,6 @@ public class HelloService {
     }
 
     public String sayHiService(String name) {
-        return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name=" + name, String.class);
+        return restTemplate.getForObject("http://CLIENT-EUREKA/hi?name=" + name, String.class);
     }
 }
